8b788a3822f2bad62088d1cd74db411917a28f5d
JAQUIER Cyprien <Cyprien.JAQUIER@cpnv.ch>
2021-01-15 11:05:23 +0100

    Script
    fin du script de création de la BD

--- SCRIPT/Creation_BDD.sql
+++ SCRIPT/Creation_BDD.sql
@@ -3,83 +3,128 @@
   Project: Darksouls
 */
 
---Create the Database
-drop database Dark_Souls
+-- Create the Database
 USE master ;
-GO 
-CREATE DATABASE Dark_souls
-/*ON PRIMARY
+drop database Darksouls
+--GO 
+CREATE DATABASE Darksouls ON PRIMARY
 ( NAME = Dark_souls_dat,
-    FILENAME = 'C:\Users\Cyprien.Jaquier\Documents\GitHub\MA-08-Modele-Darksouls\Dark_souls_dat.mdf',
-    SIZE = '10MB',
-    MAXSIZE = '50MB',
-    FILEGROWTH = '10 %' )
+    FILENAME = 'C:\Data\Cyprien_Jaquier\Darksouls_dat.mdf',
+    SIZE = 10MB,
+    MAXSIZE = 50MB,
+    FILEGROWTH = 10MB )
     LOG ON  
 ( NAME =  Dark_souls_log,  
-    FILENAME = 'C:\Users\Cyprien.Jaquier\Documents\GitHub\MA-08-Modele-Darksouls\Dark_souls_log.ldf',  
-    SIZE = '5MB',  
-    MAXSIZE = '25MB',  
-    FILEGROWTH = '5MB' ) ;  
-GO 
-*/
+    FILENAME = 'C:\Data\Cyprien_Jaquier\Darksouls_log.ldf',  
+    SIZE = 5MB,  
+    MAXSIZE = 25MB,  
+    FILEGROWTH = 5MB ) ;  
+--GO 
 
- USE Dark_souls;
 
---Create the accounts table
-CREATE TABLE accounts
+ USE Darksouls;
+-- drop table characters ;
+-- Create the accounts table
+ CREATE TABLE accounts
 	( id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	  pseudo varchar (45) NOT NULL,
 	  mail varchar (70) NOT NULL,
-	  "password" varchar (45) NOT NULL )
+	  passwords varchar (45) NOT NULL )
 
---Create the areas table
+-- Create the areas table
  CREATE TABLE areas
 	( id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	  "name" varchar (45) UNIQUE NOT NULL,
 	  bosses int,
 	  bonfires int,
-	  bonfires_id int NOT NULL )						--FK
+	  bonfires_id int NOT NULL )	-- FK
 
---Create the areas_has_monsters table
+-- Create the monsters table
+ CREATE TABLE monsters
+	( id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	  "type" int NOT NULL,
+	  boss bit NOT NULL,
+	  "name" varchar (45) NOT NULL,
+	  health int NOT NULL,
+	  damage int NOT NULL )
+
+-- Create the areas_has_monsters table
 CREATE TABLE areas_has_monsters
-	( areas_id int NOT NULL PRIMARY KEY,
-	  monsters_id int NOT NULL PRIMARY KEY )
+	( areas_id int NOT NULL,	-- FK
+	  monsters_id int NOT NULL )	-- FK
 
---Create the armors table
+-- Create the armors table
  CREATE TABLE armors
 	( id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	  "name" varchar (45) UNIQUE NOT NULL,
 	  vigor int NOT NULL,
 	  resistance int NOT NULL )
 
---Create the bonfires table
+-- Create the bonfires table
  CREATE TABLE bonfires
 	( id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	  "name" varchar (45) NOT NULL,
 	  number int UNIQUE NOT NULL )
 	  
---Create the characters table
+-- Create the characters table
  CREATE TABLE characters
 	( id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	  "name" varchar (45) UNIQUE NOT NULL,
-	  accounts_id int NOT NULL,							--FK
-	  statistics_id int NOT NULL,						--FK
-	  progressions int NOT NULL,)						--FK
+	  accounts_id int NOT NULL,							-- FK
+	  statistics_id int NOT NULL,						-- FK
+	  progressions_id int NOT NULL,)						-- FK
 
---Create the monsters table
- CREATE TABLE monsters
+-- Create the progressions table
+ CREATE TABLE progressions
 	( id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	  "type" int NOT NULL,
-	  boss bit NOT NULL,
-	  "name" varchar (45) NOT NULL,
-	  health int NOT NULL,
+	  nbBonfire int,
+	  nbBoss int,
+	  bonfire_id int NOT NULL )							-- FK
+	  
+-- Create the statistics table
+ CREATE TABLE "statistics"
+	( id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	  "type" varchar (45) NOT NULL UNIQUE,
+	  "value" int)
+
+-- Create the statistics_has_armors table
+CREATE TABLE statistics_has_armors
+	( statistics_id int NOT NULL,			-- FK
+	  armors_id int NOT NULL )				-- FK
+
+	  -- Create the armors table
+ CREATE TABLE weapons
+	( id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	  "name" varchar (45) UNIQUE NOT NULL,
+	  "type" varchar (45),
+	  strenght int NOT NULL,
 	  damage int NOT NULL )
 
---Create the monsters table
- CREATE TABLE monsters
-	( id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
-	  "type" int NOT NULL,
-	  boss bit NOT NULL,
-	  "name" varchar (45) NOT NULL,
-	  health int NOT NULL,
-	  damage int NOT NULL )
+-- Create the statistics_has_weapons table
+CREATE TABLE statistics_has_weapons
+	( statistics_id int NOT NULL,			-- FK
+	  weapons_id int NOT NULL )				-- FK
+
+GO
+
+ USE Darksouls;
+
+ALTER TABLE areas WITH CHECK ADD  CONSTRAINT FK_Darksouls_bonfires FOREIGN KEY(bonfires_id)
+REFERENCES bonfires(id)
+
+ALTER TABLE areas_has_monsters WITH CHECK ADD  CONSTRAINT FK_Darksouls_areas FOREIGN KEY(areas_id)
+REFERENCES areas(id)
+
+ALTER TABLE areas_has_monsters WITH CHECK ADD  CONSTRAINT FK_Darksouls_monsters FOREIGN KEY(monsters_id)
+REFERENCES monsters(id)
+
+ALTER TABLE characters WITH CHECK ADD  CONSTRAINT FK_Darksouls_accounts FOREIGN KEY(accounts_id)
+REFERENCES accounts(id)
+
+ALTER TABLE characters WITH CHECK ADD  CONSTRAINT FK_Darksouls_statistics FOREIGN KEY(statistics_id)
+REFERENCES "statistics"(id)
+
+ALTER TABLE characters WITH CHECK ADD  CONSTRAINT FK_Darksouls_progressions FOREIGN KEY(progressions_id)
+REFERENCES progressions(id)
+
+USE master ;
