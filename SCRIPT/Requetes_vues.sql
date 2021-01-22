a2c4e8df3868927c84acb4c62fffa4502bf12fc8
JAQUIER Cyprien <Cyprien.JAQUIER@cpnv.ch>
2021-01-22 09:07:58 +0100

    Views

---
+++ SCRIPT/Requetes_vues.sql
@@ -0,0 +1,15 @@
+use Darksouls
+-- Pour les Vues
+
+SELECT * FROM V_accounts;
+SELECT * FROM V_areas;
+SELECT * FROM V_areas_live_monsters;
+SELECT * FROM V_armors;
+SELECT * FROM V_armors_need_statistics;
+SELECT * FROM V_bonfires;
+SELECT * FROM V_characters;
+SELECT * FROM V_monsters;
+SELECT * FROM V_progressions;
+SELECT * FROM V_statistics;
+SELECT * FROM V_weapons;
+SELECT * FROM V_weapons_need_statistics;
